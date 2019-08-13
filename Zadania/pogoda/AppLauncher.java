7e6f22e6153b0b124fa9d0476a659e2b77a3426e
Tomasz Filo Zegarlicki <tzegarlicki@gmail.com>
2019-08-13 21:42:28 +0200

    Load basic file

---
+++ Zadania/pogoda/AppLauncher.java
@@ -0,0 +1,10 @@
+package ZadaniaWlasne.Zadania.pogoda;
+
+/**
+ * @author Tomasz Filo Zegarlicki
+ * [https://github.com/FiloPL]
+ * @date : 13.08.2019 21:35
+ */
+public class AppLauncher {
+
+}
---
+++ Zadania/pogoda/MainApp.java
@@ -0,0 +1,12 @@
+package ZadaniaWlasne.Zadania.pogoda;
+
+/**
+ * @author Tomasz Filo Zegarlicki
+ * [https://github.com/FiloPL]
+ * @date : 13.08.2019 21:35
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
