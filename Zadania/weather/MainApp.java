6265c71ae4d2f9dcb22e83622ab9247e69086874
Tomasz Filo Zegarlicki <tzegarlicki@gmail.com>
2019-08-14 19:05:15 +0200

    v2.03

---
+++ Zadania/weather/MainApp.java
@@ -0,0 +1,12 @@
+package ZadaniaWlasne.Zadania.weather;
+
+/**
+ * @author Tomasz Filo Zegarlicki
+ * [https://github.com/FiloPL]
+ * @date : 14.08.2019 19:01
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Weather");
+    }
+}
